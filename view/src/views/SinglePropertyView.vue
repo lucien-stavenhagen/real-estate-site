a379893c29a9d55659a7d2c783b71501e4da9632
luciens6669 <lstavenhagen@hotmail.com>
2019-11-30 22:25:53 -0600

    add v-container in single property view

--- view/src/views/SinglePropertyView.vue
+++ view/src/views/SinglePropertyView.vue
@@ -1,42 +1,44 @@
 <template>
-  <v-card v-if="this.property" outlined hover :loading="this.propLoading">
-    <v-carousel hide-delimiters>
-      <v-carousel-item :key="i" v-for="(image, i) in property.images">
-        <v-card height="100%">
-          <v-img class="align-end" height="100%" contain :src="image.source">
-            <v-card-title class="justify-center">
-              {{
-              image.caption
-              }}
-            </v-card-title>
-          </v-img>
-        </v-card>
-      </v-carousel-item>
-    </v-carousel>
-    <v-card-title v-if="property.rent">${{property.rent}} / {{property.basis}}</v-card-title>
-    <v-card-title v-else>${{property.price}}</v-card-title>
-    <v-card-text>
-      {{property.location.address}}
-      <br />
-      {{ property.location.city }},{{property.location.state}}
-    </v-card-text>
+  <v-container>
+    <v-card v-if="this.property" outlined hover :loading="this.propLoading">
+      <v-carousel hide-delimiters>
+        <v-carousel-item :key="i" v-for="(image, i) in property.images">
+          <v-card height="100%">
+            <v-img class="align-end" height="100%" contain :src="image.source">
+              <v-card-title class="justify-center">
+                {{
+                image.caption
+                }}
+              </v-card-title>
+            </v-img>
+          </v-card>
+        </v-carousel-item>
+      </v-carousel>
+      <v-card-title v-if="property.rent">${{property.rent}} / {{property.basis}}</v-card-title>
+      <v-card-title v-else>${{property.price}}</v-card-title>
+      <v-card-text>
+        {{property.location.address}}
+        <br />
+        {{ property.location.city }},{{property.location.state}}
+      </v-card-text>
 
-    <v-card-text v-if="property.acreage">Acreage: {{property.acreage}}</v-card-text>
-    <v-card-text v-if="property.squarefeet">Ft/sq: {{property.squarefeet}}</v-card-text>
-    <v-card-text v-if="property.electric === true">Electricity available? Yes</v-card-text>
-    <v-card-text v-if="property.electric === false">Electricity available? No</v-card-text>
-    <v-card-text v-if="property.plumbing === true">Plumbing available? Yes</v-card-text>
-    <v-card-text v-if="property.plumbing === false">Plumbing available? No</v-card-text>
-    <v-card-text v-if="property.beds">Beds: {{property.beds}}</v-card-text>
-    <v-card-text v-if="property.baths">Baths: {{property.baths}}</v-card-text>
-    <v-card-text v-if="property.allbillspaid === true">All bills paid? Yes</v-card-text>
-    <v-card-text v-if="property.allbillspaid === false">All bills paid? No</v-card-text>
-    <v-card-text>{{ property.description }}</v-card-text>
-    <v-card-actions>
-      <v-btn outlined to="/quicksearch">Search</v-btn>
-      <v-btn outlined to="/allpropsview">Main View</v-btn>
-    </v-card-actions>
-  </v-card>
+      <v-card-text v-if="property.acreage">Acreage: {{property.acreage}}</v-card-text>
+      <v-card-text v-if="property.squarefeet">Ft/sq: {{property.squarefeet}}</v-card-text>
+      <v-card-text v-if="property.electric === true">Electricity available? Yes</v-card-text>
+      <v-card-text v-if="property.electric === false">Electricity available? No</v-card-text>
+      <v-card-text v-if="property.plumbing === true">Plumbing available? Yes</v-card-text>
+      <v-card-text v-if="property.plumbing === false">Plumbing available? No</v-card-text>
+      <v-card-text v-if="property.beds">Beds: {{property.beds}}</v-card-text>
+      <v-card-text v-if="property.baths">Baths: {{property.baths}}</v-card-text>
+      <v-card-text v-if="property.allbillspaid === true">All bills paid? Yes</v-card-text>
+      <v-card-text v-if="property.allbillspaid === false">All bills paid? No</v-card-text>
+      <v-card-text>{{ property.description }}</v-card-text>
+      <v-card-actions>
+        <v-btn outlined to="/quicksearch">Search</v-btn>
+        <v-btn outlined to="/allpropsview">Main View</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
 <script>
 import { mapGetters } from "vuex";
